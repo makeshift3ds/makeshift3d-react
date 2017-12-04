4be20548387f1a205259957d4d7051c7335239c9
Ken Elliott <makeshift3ds@gmail.com>
2017-12-04 12:22:01 -0500

    add a default webpack config

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+import path from 'path';
+// const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    app: path.join(__dirname, 'index.js')
+  },
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'build.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack demo'
+    })
+  ]
+};
