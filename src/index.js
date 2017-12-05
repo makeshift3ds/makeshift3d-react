155c7b7dc2232d582eb4111dc90f69a78b7c600e
Ken Elliott <makeshift3ds@gmail.com>
2017-12-05 10:26:04 -0500

    remove manual hmr implementation (it was for science)

--- src/index.js
+++ src/index.js
@@ -7,14 +7,3 @@ function component() {
 }
 
 component();
-
-if (module.hot) {
-  console.log('is hot');
-  module.hot.accept('./hello.js', function() {
-    console.log('Accepting the updated hello module!');
-
-    document.body.removeChild(res);
-    res = component();
-    document.body.appendChild(res);
-  });
-}
