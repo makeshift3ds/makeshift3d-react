3bf63a473ec878f58d4f9e756b02b2f5fe84fda8
Ken Elliott <makeshift3ds@gmail.com>
2017-12-07 08:46:22 -0500

    moved config files

--- webpack.config.js
+++ webpack.config.js
@@ -7,42 +7,42 @@ const DashboardPlugin = require('webpack-dashboard/plugin');
 const merge = require('webpack-merge');
 
 /* Configurations */
-const cssConfig = require('./webpack/css.config');
-const devServerConfig = require('./webpack/dev-server.config');
-const extractCssConfig = require('./webpack/extract-css.config');
-const imgConfig = require('./webpack/img.config');
-const sourceMapConfig = require('./webpack/source-maps.config');
-const cleanConfig = require('./webpack/clean.config');
-const uglifyConfig = require('./webpack/uglify.config');
-const jsConfig = require('./webpack/js.config');
+const cssConfig = require('./config/webpack/css.config');
+const devServerConfig = require('./config/webpack/dev-server.config');
+const extractCssConfig = require('./config/webpack/extract-css.config');
+const imgConfig = require('./config/webpack/img.config');
+const sourceMapConfig = require('./config/webpack/source-maps.config');
+const cleanConfig = require('./config/webpack/clean.config');
+const uglifyConfig = require('./config/webpack/uglify.config');
+const jsConfig = require('./config/webpack/js.config');
 
 /* Paths */
 const PATHS = {
   src: path.join(__dirname, 'src'),
-  build: path.join(__dirname, 'build'),
+  build: path.join(__dirname, 'build')
 };
 
 /* Common Configuration */
 const common = merge(
   {
     entry: {
-      app: PATHS.src,
+      app: PATHS.src
     },
     output: {
       path: PATHS.build,
-      filename: 'index.bundle.js',
+      filename: 'index.bundle.js'
     },
     plugins: [
       new DashboardPlugin(),
       new HtmlWebpackPlugin({
         title: 'React App',
-        template: './src/index.html',
+        template: './src/index.html'
       }),
-      new webpack.NamedModulesPlugin(),
+      new webpack.NamedModulesPlugin()
     ],
     resolve: {
-      extensions: ['.js', '.jsx'],
-    },
+      extensions: ['.js', '.jsx']
+    }
   },
   imgConfig.load(),
   jsConfig.load()
@@ -56,7 +56,7 @@ module.exports = env => {
       sourceMapConfig.load({ devtool: 'source-map' }),
       cleanConfig.load({
         root: process.cwd(),
-        path: PATHS.build,
+        path: PATHS.build
       }),
       uglifyConfig.load()
     );
