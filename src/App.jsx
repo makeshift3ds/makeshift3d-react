aa5ff980e49274aa2dafa9fad73384db233979c2
Ken Elliott <makeshift3ds@gmail.com>
2017-12-07 08:46:59 -0500

    App html layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,55 @@
 import React from 'react';
 
-const App = () => <div>Testing</div>;
+let time = Date.now();
+
+const App = () => (
+  <div className="app">
+    <div>Ken Elliott</div>
+    <nav className="routes">
+      <ul>
+        <li>
+          <a href="#1">Home</a>
+        </li>
+        <li>
+          <a href="#2">Blog</a>
+        </li>
+        <li>
+          <a href="#3">Resume</a>
+        </li>
+        <li>
+          <a href="#4">Contact</a>
+        </li>
+      </ul>
+    </nav>
+
+    <section className="hero">
+      <div className="avatar">Me</div>
+      <nav className="social">
+        <ul>
+          <li>
+            <a href="#github">github</a>
+          </li>
+          <li>
+            <a href="#linked">linkedin</a>
+          </li>
+          <li>
+            <a href="#twitter">twitter</a>
+          </li>
+          <li>
+            <a href="#youtube">youtube</a>
+          </li>
+        </ul>
+      </nav>
+    </section>
+
+    <article className="content">
+      <h1>Home</h1>
+    </article>
+
+    <footer>
+      <div>Copyright {time}</div>
+    </footer>
+  </div>
+);
 
 export default App;
