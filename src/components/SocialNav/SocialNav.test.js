772976cfab64a4c15b78800fe978ad3f42d233e8
Ken Elliott <makeshift3ds@gmail.com>
2017-12-11 12:05:24 -0500

    red/green <SocialNav /> component

---
+++ src/components/SocialNav/SocialNav.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import SocialNav from './SocialNav.jsx';
+
+describe('<SocialNav />', () => {
+  it('should render 1 <SocialNav /> component', () => {
+    const wrapper = shallow(<SocialNav />);
+    expect(wrapper).toHaveLength(1);
+  });
+
+  it('should render an avatar element', () => {
+    const component = shallow(<SocialNav />);
+    expect(component.find('.social-nav')).toHaveLength(1);
+  });
+
+  it('should match snapshot', () => {
+    const component = shallow(<SocialNav />);
+    expect(component).toMatchSnapshot();
+  });
+});
