d5096eaf074df55e5de3eae3b0e9b84c917fd25a
Ken Elliott <makeshift3ds@gmail.com>
2017-12-11 14:11:38 -0500

    add transform-class-properties and babel-polyfill for async/await support

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import 'babel-polyfill';
 
 export default class Blog extends Component {
+  state = { posts: [] };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('http://jsonplaceholder.typicode.com/posts');
+      const posts = await res.json();
+      this.setState({ posts });
+    } catch (e) {
+      console.log('error', e);
+    }
+  }
+
   render() {
     return (
       <div className="content-container">
-        <div>Hello</div>
+        <article>
+          {this.state.posts.length &&
+            this.state.posts.map(post => <div key={post.id}>{post.title}</div>)}
+        </article>
+        <aside>Aside!</aside>
       </div>
     );
   }
--- src/components/Blog/Blog.test.js
+++ src/components/Blog/Blog.test.js
@@ -2,9 +2,30 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Blog from './Blog.jsx';
 
+const mockFetch = () =>
+  new Promise(resolve => {
+    resolve({
+      json() {
+        return [{ id: 1, title: 'foo' }];
+      }
+    });
+  });
+
 describe('<Blog />', () => {
   it('should render 1 <Blog /> component', () => {
     const component = shallow(<Blog />);
     expect(component).toHaveLength(1);
+  });
+
+  it('should have an empty array as a default state', () => {
+    const component = shallow(<Blog />);
+    expect(component.state().posts).toEqual([]);
+  });
+
+  it('should call fetch to get posts', () => {
+    global.fetch = jest.fn().mockImplementation(mockFetch);
+    shallow(<Blog />);
+    expect(global.fetch).toBeCalledWith('http://jsonplaceholder.typicode.com/posts');
+    expect(global.fetch).toHaveBeenCalledTimes(1);
   });
 });
