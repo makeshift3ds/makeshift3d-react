1b955291a81f8517cc02b9a5dfee203d6f383597
Ken Elliott <makeshift3ds@gmail.com>
2017-12-14 09:13:32 -0500

    data request for home first pass

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,19 +1,43 @@
 import React from 'react';
 import './Home.css';
 
-const Home = () => (
-  <div className="content-container">
-    <article>
-      <h1>Home</h1>
-      <p>This is some stuff about me</p>
-    </article>
-    <aside>
-      <div className="aside-block">
-        <p>This guy codes!</p>
-        <span> - Random Redditor</span>
-      </div>
-    </aside>
-  </div>
-);
+/* polyfill for async await */
+import BabelPolyfill from 'babel-polyfill';
 
-export default Home;
+export default class Home extends React.Component {
+  async componentDidMount() {
+    try {
+      const res = await fetch('http://localhost:3000/pages/foobar');
+      const page = await res.json();
+      this.setState({
+        page,
+      });
+    } catch (e) {
+      console.log('error', e);
+    }
+  }
+
+  state = {
+    page: {},
+  };
+
+  render() {
+    const { title, body } = this.state.page;
+
+    return (
+      <div className="content-container">
+        <article>
+          <h1>{title}</h1>
+          <p>{body}</p>
+        </article>
+        <aside>
+          <div className="aside-block">
+            <p>This guy codes!</p>
+            <span> - Random Redditor</span>
+          </div>
+        </aside>
+      </div>
+    );
+  }
+}
+
