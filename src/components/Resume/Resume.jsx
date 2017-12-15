4953d9a28850b9ac667e585398a548fbaf036593
Ken Elliott <makeshift3ds@gmail.com>
2017-12-15 12:32:55 -0500

    remove commented code from resume

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -39,23 +39,3 @@ export default class Home extends React.Component {
     );
   }
 }
-
-// import React from 'react';
-// import './Resume.css';
-
-// const Resume = () => (
-//   <div className="content-container">
-//     <article>
-//       <h1>Resume</h1>
-//       <p>This is about my professional career</p>
-//     </article>
-//     <aside>
-//       <div className="aside-block">
-//         <p>This guy codes!</p>
-//         <span> - Random Redditor</span>
-//       </div>
-//     </aside>
-//   </div>
-// );
-
-// export default Resume;
