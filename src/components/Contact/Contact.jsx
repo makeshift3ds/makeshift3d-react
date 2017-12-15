f854b5c5ecf761fbbe8c91b19539953d19f27670
Ken Elliott <makeshift3ds@gmail.com>
2017-12-15 09:52:08 -0500

    remove commented code in contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -39,24 +39,3 @@ export default class Home extends React.Component {
     );
   }
 }
-
-
-// import React from 'react';
-// import './Contact.css';
-
-// const Contact = () => (
-//   <div className="content-container">
-//     <article>
-//       <h1>Contact</h1>
-//       <p>This is how to contact me</p>
-//     </article>
-//     <aside>
-//       <div className="aside-block">
-//         <p>This guy codes!</p>
-//         <span> - Random Redditor</span>
-//       </div>
-//     </aside>
-//   </div>
-// );
-
-// export default Contact;
